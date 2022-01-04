839596597315a928c45c8e668ac64b92ebbb26c1
Lev Eche <projects+catchall@semyon.org>
2022-01-04 03:11:14 +0000

    ad hoc Aufgabe 1.3

---
+++ AhA1p1.java
@@ -0,0 +1,9 @@
+// hello world
+
+public class AhA1p1 {
+
+  public static void main(String[] args) {
+      System.out.println ("Hello World");
+  }
+
+}
---
+++ AhA1p2.java
@@ -0,0 +1,12 @@
+// hello world
+
+public class AhA1p2 {
+
+  public static void main(String[] args) {
+      System.out.println ("Hello, " + args[0] + " " + args[1]);
+  }
+
+}
+
+// if not enough parameters are given, an exception is thrown:
+// Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 1 out of bounds for length 1
